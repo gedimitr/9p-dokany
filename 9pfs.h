0d4534ba376c61937d342c12172c42c83f7e6371
Gerasimos Dimitriadis <gedimitr@gmail.com>
2020-09-22 00:52:20 +0300

    Add comment header in source files about license / copyright

--- 9pfs.h
+++ 9pfs.h
@@ -1,7 +1,24 @@
-#ifndef _9PFS_H_
-#define _9PFS_H_
+/*
+ * This file is part of 9p-dokany <https://github.com/gedimitr/9p-dokany>.
+ *
+ * 9p-dokany is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 9p-dokany is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 9p-dokany. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2020 Gerasimos Dimitriadis
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#pragma once
 
 #include <dokan/dokan.h>
 #include <dokan/fileinfo.h>
-
-#endif  // _9PFS_H_
